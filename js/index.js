a0401c5d6f73486747530105ad325f1fed54ea3d
catmac <christiantabile@gmail.com>
2022-07-12 08:24:54 -0700

    Edit js files for Task implementation

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 
 var tasks = new TaskManager()
-
 console.log(tasks);
 
-window.addEventListener("load",() =>{
+window.addEventListener("load", () => {
 
-    
-    const form = document.querySelector(".form");
-    const taskName = document.querySelector("#taskName"); 
-    const taskStatus = document.querySelector("#taskStatus"); 
-    const taskAssign = document.querySelector("#taskAssign"); 
-    const taskDueDate = document.querySelector("#taskDueDate"); 
-    const taskDescription = document.querySelector("#taskDescription"); 
-    
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-    
+  const form = document.querySelector(".form");
+  const taskName = document.querySelector("#taskName");
+  const taskStatus = document.querySelector("#taskStatus");
+  const taskAssign = document.querySelector("#taskAssign");
+  const taskDueDate = document.querySelector("#taskDueDate");
+  const taskDescription = document.querySelector("#taskDescription");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     /*const enteredName = document.createElement("input");
     enteredName.classList.add(taskName);
     enteredName.type = "text";
@@ -25,22 +23,26 @@ window.addEventListener("load",() =>{
     // append
     form.appenchild("enteredName");
     */
-   if(taskName == null || taskName == null|| taskStatus == null || taskAssign == null || taskDueDate == null || taskDescription == null) {
-    alert("Please fill out all required fields")
-   } else{
-    tasks.addTask();
-   }
-    
-    
-    
-    
-    
+    if (taskName == null || taskName == null || taskStatus == null || taskAssign == null || taskDueDate == null || taskDescription == null) {
+      alert("Please fill out all required fields")
+    } else {
+      const name = taskName.value;
+      const description = taskDescription.value;
+      const assignedTo = taskAssign.value;
+      const dueDate = taskDueDate.value;
+
+
+      tasks.addTask(name, description, assignedTo, dueDate);
+    }
+
+    tasks.render();
+
     form.reset();
 
-    
-      //end of submit event listener
-    })
+
+    //end of submit event listener
+  })
 
 
-    //end of window load event listener
+  //end of window load event listener
 })
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,10 +1,34 @@
+
+function createTaskHtml(name, description, assignedTo, dueDate, status) {
+    const html = `
+    <li class="list-group-item">
+        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+            <h5>${name}/h5>
+            <span class="badge ${status === 'TODO' ? 'badge-danger' : 'badge-success'}">${status}</span>
+        </div>
+        <div class="d-flex w-100 mb-3 justify-content-between">
+            <small>Assigned To: ${assignedTo}</small>
+            <small>Due: ${dueDate}</small>
+        </div>
+        <p>${description}</p>
+        <button type="button" class="delete-button btn btn-danger">Delete this task</button>
+        <button type="button" class="done-button btn btn-success ${status === 'TODO' ? 'visible' : 'invisible'}">Mark as done</button>
+    </li>
+`;
+
+    return html;
+}
+
+const taskHtml = createTaskHtml('Take out the trash', 'Take out the trash to the front of the house', 'Nick', '2020-09-20', 'TODO');
+console.log(taskHtml);
+
 class TaskManager {
-    constructor(currentId = 0){
-      this.tasks = [];
-      this.currentId = currentId;
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
 
     }
-    addTask(name,description,assignedTo,dueDate,status){
+    addTask(name, description, assignedTo, dueDate) {
         this.currentId++
         const task1 = {
             id: this.currentId,
@@ -17,9 +41,21 @@ class TaskManager {
         this.tasks.push(task1);
     }
 
+    render() {
+        var tasksHtmlList = [];
+
+        for (let i = 0; i < this.tasks.length; i++) {
+            var currentTask = this.tasks[i];
+            const getDate = newDate(currentTask.dueDate);
+            const formattedDate = (newDate.getMonth() + 1) + '/' + newDate.getDate() + '/' + newDate.getFullYear();
+
+            var taskHtml = createTaskHtml(currentTask.id, currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status);
+            tasksHtmlList.push(taskHtml);
+
+            var tasksHtml = tasksHtmlList.join('\n');
+            var tasksList = document.querySelector('#tasksList');
+            tasksList.innerHTML = tasksHtml;
+        }
+    }
+
 }
-
-
-
-
-
