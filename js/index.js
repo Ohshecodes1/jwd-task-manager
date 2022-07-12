ccb7b5a94feda76c6123d20554edda6e4956ab84
GC <contact@gcteam.dev>
2022-07-12 09:30:27 -0700

    JS form validation altered

--- js/index.js
+++ js/index.js
@@ -5,32 +5,18 @@ console.log(tasks);
 window.addEventListener("load", () => {
 
   const form = document.querySelector(".form");
-  const taskName = document.querySelector("#taskName");
-  const taskStatus = document.querySelector("#taskStatus");
-  const taskAssign = document.querySelector("#taskAssign");
-  const taskDueDate = document.querySelector("#taskDueDate");
-  const taskDescription = document.querySelector("#taskDescription");
+  const name = document.querySelector("#taskName").value;
+  const assignedTo = document.querySelector("#taskAssign").value;
+  const dueDate = document.querySelector("#taskDueDate").value;
+  const description = document.querySelector("#taskDescription").value;
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    /*const enteredName = document.createElement("input");
-    enteredName.classList.add(taskName);
-    enteredName.type = "text";
-    enteredName.value = taskName.input;
-    enteredName.setattribute = "readonly","readonly";
-
-    // append
-    form.appenchild("enteredName");
-    */
-
-    if (taskName == null || taskName == null || taskStatus == null || taskAssign == null || taskDueDate == null || taskDescription == null) {
+    
+    if (!name || !description || !assignedTo || !dueDate ) {
       alert("Please fill out all required fields")
     } else {
-      const name = taskName.value;
-      const description = taskDescription.value;
-      const assignedTo = taskAssign.value;
-      const dueDate = taskDueDate.value;
+    
 
 
       tasks.addTask(name, description, assignedTo, dueDate);
